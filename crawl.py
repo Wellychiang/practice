e9cde101e0f79681143917002d2efd96230eb254
Wellychiang <g45p2k7a8@jiangweixiaodeMacBook-Air.local>
2020-08-03 01:01:53 +0800

    selenium

--- crawl.py
+++ crawl.py
@@ -3,6 +3,7 @@ from lxml import etree
 from pprint import pprint
 import json
 import re
+from selenium import webdriver
 
 
 def crawl(urls):
@@ -10,18 +11,33 @@ def crawl(urls):
         yield len(res.content), res.status_code, res.url
 
 
-url = 'https://nlnbamdnyc-a.akamaihd.net/fs/nba/feeds_s2019/schedule/2020/7_27.js'
-headers = {
-    'Referer': 'https://watch.nba.com/',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 '
-                  '(KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'
-}
-data = {'t': '1596362940000'}
-qw = requests.get(url, headers=headers, params=data)
-a = re.findall('g_schedule=(.*)', qw.text)
+def req_nba():
+    url = 'https://nlnbamdnyc-a.akamaihd.net/fs/nba/feeds_s2019/schedule/2020/7_27.js'
+    headers = {
+        'Referer': 'https://watch.nba.com/',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 '
+                      '(KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'
+    }
+    data = {'t': '1596362940000'}
+    qw = requests.get(url, headers=headers, params=data)
+    a = re.findall('g_schedule=(.*)', qw.text)
 
-count = -1
-for i in range(6):
-    count += 1
-    one = {k: v for k, v in json.loads(a[0])['games'][6][count].items()}
-    print(one['h'] + ' vs ' + one['v'], one['hr'], count)
+    count = -1
+    for i in range(6):
+        count += 1
+        one = {k: v for k, v in json.loads(a[0])['games'][6][count].items()}
+        print(one['h'] + ' vs ' + one['v'], one['hr'], count)
+
+
+#  Chromedriver在下載資料夾, 還沒加入環境變數
+def selenium_nba():
+    b = webdriver.Chrome()
+    b.implicitly_wait(3)
+    b.get('https://watch.nba.com/')
+    game = b.find_element_by_xpath("//*[contains(span,'比賽')]")
+    game.click()
+    page = b.page_source
+    with open('source.txt', 'a') as file:
+        print(page, file=file)
+
+
